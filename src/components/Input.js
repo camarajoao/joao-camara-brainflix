a2bec4d6a40ff6414f8f10bab1e17667a7f1d8d4
jcamara <camarajoao1@gmail.com>
2023-02-28 16:31:24 -0800

    create input component and style sheet

---
+++ src/components/Input.js
@@ -0,0 +1,8 @@
+import "../styles/partials/Input.scss";
+import SearchIcon from "./SearchIcon.js"
+
+export default function Input() {
+    return (
+        <input type="search" placeholder="Search" class="search" />
+    )
+}
