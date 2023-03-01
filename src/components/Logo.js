47e5e9909385a045f75554cd301eeb3564f70ddb
jcamara <camarajoao1@gmail.com>
2023-02-28 16:31:42 -0800

    create logo component and style sheet

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import "../styles/partials/Logo.scss";
+import brainFlixLogo from "../assets/logo/BrainFlix-logo.svg";
+
+export default function Logo() {
+    return (
+        <div>
+            <img src={brainFlixLogo} />
+        </div>
+    )
+}
