af1ab68682e93b7206f247b3762df8757c183def
jcamara <camarajoao1@gmail.com>
2023-02-28 16:32:05 -0800

    create search bar div component and style sheet

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import "../styles/partials/SearchBar.scss";
+import Input from "./Input";
+import SearchIcon from "./SearchIcon";
+
+export default function SearchBar() {
+    return (
+        <div class="searchBar">
+        <Input />
+        <SearchIcon />
+        </div>
+    )
+}
