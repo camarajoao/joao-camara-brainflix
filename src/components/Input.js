3a3fca1b3932155fcb38e405982b4ca2e96b5980
jcamara <camarajoao1@gmail.com>
2023-03-01 16:41:00 -0800

    deleted files

--- src/components/Input.js
+++
@@ -1,8 +0,0 @@
-import "../styles/partials/Input.scss";
-import SearchIcon from "./SearchIcon.js"
-
-export default function Input() {
-    return (
-        <input type="search" placeholder="Search" class="search" />
-    )
-}
