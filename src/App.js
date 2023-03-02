21b033a9bb88f72ec5b8e9c8754faa43273b9fa6
jcamara <camarajoao1@gmail.com>
2023-03-01 19:33:12 -0800

    add media to app js and mixin to scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./components/Header/Header.scss";
 import Header from "./components/Header/Header";
+import Media from "./components/Media/Media"
 
 function App() {
   return (
-    <Header />
+    <div className="App">
+      <Header />
+      <Media />
+    </div>
   );
 }
 
