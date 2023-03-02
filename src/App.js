2b727e58d27119de7ac405e270dadb0a0393fa4c
jcamara <camarajoao1@gmail.com>
2023-03-01 21:28:31 -0800

    add comments section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./components/Header/Header.scss";
 import Header from "./components/Header/Header";
-import Media from "./components/Media/Media"
+import Media from "./components/Media/Media";
+import Comments from "./components/Comments/Comments"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Media />
+      <Comments />
     </div>
   );
 }
