d5f53f3ff17043849fd46def47d287d794a8b8ea
jcamara <camarajoao1@gmail.com>
2023-03-03 15:21:28 -0800

    add video component to app js and add divs to style page to desktop version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import "./components/Header/Header.scss";
 import Header from "./components/Header/Header";
+import Video from "./components/Video/Video"
 import Media from "./components/Media/Media";
 import Comments from "./components/Comments/Comments";
 import NextVideos from "./components/NextVideos/NextVideos";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Media />
-      <Comments />
-      <NextVideos />
+      <Video />
+      <div className="App__container">
+        <div className="App__Interaction">
+          <Media />
+          <Comments />
+        </div>
+        <div className="App__nextVideos">
+          <NextVideos />
+        </div>
+      </div>
     </div>
   );
 }
