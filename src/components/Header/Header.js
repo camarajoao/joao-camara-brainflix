7b39b1da65878df0741fb35ff1b59d977aecf430
jcamara <camarajoao1@gmail.com>
2023-03-02 16:18:22 -0800

    rename class to className. add a new avatar to header to display only on tablet and desktop. style header for tablet and desktop.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,20 +6,23 @@ import uploadIcon from "../../assets/icons/upload.svg"
 
 export default function Header() {
     return (
-        <header class="header">
-            <div class="header__logo">
-                <img src={brainFlixLogo} alt="brainflix logo" />
+        <header className="header">
+            <div className="header__logoContainer">
+                <img className="header__logo" src={brainFlixLogo} alt="brainflix logo" />
             </div>
-            <div class="header__searchBar">
-                <input class="header__input" type="search" placeholder="Search" />
-                <button class="header__searchIcon" type="submit">
+            <div className="header__searchBar">
+                <input className="header__input" type="search" placeholder="Search" />
+                <button className="header__searchIcon" type="submit">
                     <img src={searchIcon} alt="search-icon" />
                 </button>
             </div>
-            <div class="header__avatarContainer">
-                <img src={avatar} class="header__avatarImage" alt="mohan muruge" />
+            <div className="header__avatarContainer">
+                <img src={avatar} className="header__avatarImage" alt="mohan muruge" />
             </div>
-            <button class="header__button" type="submit"><span class="header__uploadContainer"><img src={uploadIcon} class="header__upload" /></span>UPLOAD</button>
+            <button className="header__button" type="submit"><span className="header__uploadContainer"><img src={uploadIcon} className="header__upload" /></span>UPLOAD</button>
+            <div className="header__avatarSecondContainer">
+                <img src={avatar} className="header__avatarImage" alt="mohan muruge" />
+            </div>
         </header>
     )
 }
