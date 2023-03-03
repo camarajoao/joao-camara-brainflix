30b2ed09b8b1bd53f83e3e1d01ccf9eb395f61cf
jcamara <camarajoao1@gmail.com>
2023-03-03 15:20:33 -0800

    create video component

---
+++ src/components/Video/Video.js
@@ -0,0 +1,9 @@
+import "./Video.scss";
+
+export default function Video() {
+    return (
+        <video className="video" poster="" controls="true">
+            <source src="" />
+        </video>
+    )
+}
