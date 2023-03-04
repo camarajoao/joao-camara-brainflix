4767cf7ffb16e7e342b6f72c5356352330c67e21
jcamara <camarajoao1@gmail.com>
2023-03-03 20:22:22 -0800

    add component to display list of next videos

---
+++ src/components/NextVideosItem/NextVideosItem.js
@@ -0,0 +1,16 @@
+import "./NextVideosItem.scss";
+
+export default function NextVideosItem({ video, selectVideo }) {
+    console.log(video);
+    return (
+        <div className="nextVideosItem" onClick={() => { selectVideo(video.id) }}>
+            <div className="nextVideosItem__hero">
+                <img className="nextVideosItem__image" src={video.image} />
+            </div>
+            <div className="nextVideosItem__description">
+                <h3>{video.title}</h3>
+                <p>{video.channel}</p>
+            </div>
+        </div>
+    )
+}
