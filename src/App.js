9348b9ed35dae4a98ec7ed38b9d0aeb6111e1d79
jcamara <camarajoao1@gmail.com>
2023-03-03 20:13:24 -0800

    add function to display videos and next videos list from json file.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import "./components/Header/Header.scss";
 import Header from "./components/Header/Header";
-import Video from "./components/Video/Video"
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer"
 import Media from "./components/Media/Media";
 import Comments from "./components/Comments/Comments";
 import NextVideos from "./components/NextVideos/NextVideos";
+import videosData from "./data/videos.json";
+import videoDetailsData from "./data/video-details.json";
+import { useState } from "react";
+
 
 
 function App() {
+
+  const [videos] = useState(videosData);
+  const [selectedVideo, setSelectedVideo] = useState(videoDetailsData[0]);
+
+  function handleSelectVideo(clickedId) {
+    const foundVideo = videoDetailsData.find((video) => clickedId === video.id);
+
+    setSelectedVideo(foundVideo)
+  }
+
+  const filteredVideos = videos.filter((video) => video.id !== selectedVideo.id);
+
+
   return (
     <div className="App">
       <Header />
-      <Video />
+      <VideoPlayer selectedVideo={selectedVideo}  />
       <div className="App__container">
         <div className="App__Interaction">
           <Media />
           <Comments />
         </div>
         <div className="App__nextVideos">
-          <NextVideos />
+          <NextVideos videos={filteredVideos} selectVideo={handleSelectVideo} />
         </div>
       </div>
     </div>
