6258b879c9b17d7bceabd09a7d69ccf8b66f5030
jcamara <camarajoao1@gmail.com>
2023-03-04 20:29:30 -0800

    add comment that img cannot have alt text because of styling. rename classes on the scss file.

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -6,12 +6,13 @@ export default function Comment({ comment }) {
     const day = ("0" + date.getDate()).slice(-2);
     const month = ("0" + (date.getMonth() + 1)).slice(-2);
     const year = date.getFullYear();
-    const timestamp = `${month}/${day}/${year}`
+    const timestamp = `${month}/${day}/${year}`;
 
     return (
         <div className="comment">
             <div className="comment__avatar">
                 <img className="comment__avatar-image" />
+                {/* this image tag cannot receive an alt property or it breaks the style */}
             </div>
             <div className="comment__posted">
                 <div className="comment__posted-information">
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,33 +2,31 @@ import "./Comments.scss";
 import avatar from "../../assets/images/Mohan-muruge.jpg";
 import addCommentIcon from "../../assets/icons/add_comment.svg";
 import Comment from "../Comment/Comment";
-import { useState } from "react";
 
 export default function Comments({ selectedVideo }) {
 
     return (
-        <div className="conversation">
-            <h3 className="conversation__title">{selectedVideo.comments.length} Comments</h3>
-            <div className="conversation__container">
-                <div className="conversation__post-comment">
-                    <div className="conversation__left">
-                        <img src={avatar} className="conversation__icon" alt="mohan muruge" />
+        <div className="comments">
+            <h3 className="comments__count">{selectedVideo.comments.length} Comments</h3>
+            <div className="comments__container">
+                <div className="comments__user">
+                    <div className="comments__user-avatar">
+                        <img src={avatar} className="comments__user-avatar-image" alt="mohan muruge" />
                     </div>
-                    <div className="conversation__right">
-                        <form className="conversation__form">
-                            <label className="form__comment labels" htmlFor="comment">JOIN THE CONVERSATION</label>
-                            <textarea className="form__comment-input" name="comment" id="comment" placeholder="Add a new comment"></textarea>
-                            <button className="form__button" type="submit"><span className="form__commentIconContainer"><img src={addCommentIcon} className="form__commentIcon" alt="addcomment-icon" /></span>COMMENT</button>
+                    <div className="comments__user-post">
+                        <form className="comments__form">
+                            <label className="comments__form-label labels" htmlFor="comment">JOIN THE CONVERSATION</label>
+                            <textarea className="comments__form-input" name="comment" id="comment" placeholder="Add a new comment"></textarea>
+                            <button className="comments__form-button" type="submit"><span className="comments__form-button-submit"><img src={addCommentIcon} className="comments__form-button-submit-icon" alt="addcomment-icon" /></span>COMMENT</button>
                         </form>
                     </div>
                 </div>
-                <div className="conversation__posted">
+                <div className="comments__posted">
                     {selectedVideo.comments.map((comment) => (
-                        <Comment comment={comment} />
+                        <Comment key={comment.id} comment={comment} />
                     ))}
                 </div>
             </div>
         </div>
     )
 }
-
