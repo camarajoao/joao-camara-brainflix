aa8b3555389df2c980b44f2d551f6db5f0a06b78
jcamara <camarajoao1@gmail.com>
2023-03-08 18:38:00 -0800

    move imports and state to home js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import "./_App.scss";
 
-import { useState } from "react";
-import axios from 'axios';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import videosData from "./data/videos.json";
-import videoDetailsData from "./data/video-details.json";
 
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import Videos from "./pages/Videos/Videos";
 import VideoUpload from "./pages/VideoUpload/VideoUpload";
+import NotFound from "./pages/NotFound/NotFound";
 
 
 function App() {
-
-  const [videos] = useState(videosData);
-  const [selectedVideo, setSelectedVideo] = useState(videoDetailsData[0]);
-
-  function handleSelectVideo(clickedId) {
-    const foundVideo = videoDetailsData.find((video) => clickedId === video.id);
-
-    setSelectedVideo(foundVideo)
-  }
-
-  const filteredVideos = videos.filter((video) => video.id !== selectedVideo.id);
-
 
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Home videos={filteredVideos} selectVideo={handleSelectVideo} selectedVideo={selectedVideo} />} />
-          <Route path="/videos" element={<Videos videos={filteredVideos} selectVideo={handleSelectVideo} selectedVideo={selectedVideo} />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/videos/:videoId" element={<Home />} />
           <Route path="/upload" element={<VideoUpload />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
