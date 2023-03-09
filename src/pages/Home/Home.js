996292eedec8232717b113a80d2627c7558084e8
jcamara <camarajoao1@gmail.com>
2023-03-08 18:38:32 -0800

    move components from app js to home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,42 @@
+import "../../components/NextVideos/NextVideos.scss"
 
-import VideoPlayer from "../../components/VideoPlayer/VideoPlayer"
+import { Link, useParams } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+
+
+import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 import Media from "../../components/Media/Media";
 import Comments from "../../components/Comments/Comments";
-import NextVideos from "../../components/NextVideos/NextVideos";
+import NextVideosItem from "../../components/NextVideosItem/NextVideosItem";
+import NotFound from "../NotFound/NotFound";
 
-export default function Home({ videos, selectVideo, selectedVideo }) {
+import videosData from "../../data/videos.json";
+import videoDetailsData from "../../data/video-details.json";
+
+
+export default function Videos() {
+
+    const { videoId } = useParams();
+    let video = videoDetailsData.find((v) => v.id === videoId);
+
+
+    if (!videoId) {
+        video = videoDetailsData[0];
+    }
+
+    const [selectedVideo, setSelectedVideo] = useState(video);
+
+    if (!video) {
+        return <NotFound />
+    }
+
+    function handleSelectVideo() {
+        setSelectedVideo(video);
+    }
+    console.log(handleSelectVideo);
+    const filteredVideos = videoDetailsData.filter((v) => v.id !== selectedVideo.id);
+
     return (
         <>
             <VideoPlayer selectedVideo={selectedVideo} />
@@ -14,7 +46,16 @@ export default function Home({ videos, selectVideo, selectedVideo }) {
                     <Comments selectedVideo={selectedVideo} />
                 </div>
                 <div className="App__nextVideos">
-                    <NextVideos videos={videos} selectVideo={selectVideo} />
+                    <div className="nextVideos">
+                        <h3 className="nextVideos__title">NEXT VIDEOS</h3>
+                        <nav className="nextVideos__container">
+                            {filteredVideos.map((video) => (
+                                <Link to={`/videos/${video.id}`} key={video.id}>
+                                    <NextVideosItem key={video.id} video={video} selectVideo={handleSelectVideo} />
+                                </Link>
+                            ))}
+                        </nav>
+                    </div>
                 </div>
             </div>
         </>
